68f9b5ade6675a424831cff5caab14fd60c51b36
Kenneth Kebaara <kkebaara@yahoo.com>
2022-01-23 13:33:57 -0700

    commit tree a3de8bd8823a62d5337024d64dc71fbdd58a60d6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
+    const inputForm = document.querySelector('form')
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => {
+        /* 
+        Let's replace Title and Summary with data we retrieved from our server. 
+        To do this, we'll work inside the second then of our fetch request. 
+        First, we'll access the DOM and store the two elements in JavaScript
+        */
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+  
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
